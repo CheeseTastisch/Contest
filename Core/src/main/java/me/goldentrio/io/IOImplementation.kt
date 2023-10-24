fe9ee72b5871ec30a96cfeae1cf92f6c47fc3ae5
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-10-24 09:21:35 +0200

    Bugfix

--- Core/src/main/java/me/goldentrio/io/IOImplementation.kt
+++ Core/src/main/java/me/goldentrio/io/IOImplementation.kt
@@ -18,20 +18,6 @@ internal class IOImplementation(
         source.writeOutput(outputQueue)
     }
 
-    @Deprecated(
-        "Use readValues with join instead",
-        replaceWith = ReplaceWith("readValues(peek).joinToString(separator)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun readWholeLine(peek: Boolean) = readValues(peek).joinToString(" ")
-
-    @Deprecated(
-        "Use readValues instead",
-        replaceWith = ReplaceWith("readValues(peek)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun readLine(peek: Boolean) = readValues(peek)
-
     override fun readValue(peek: Boolean, endOfLine: Boolean): String {
         if (peek && endOfLine) throw IllegalArgumentException("peek and endOfLine can't be both set")
 
@@ -53,16 +39,6 @@ internal class IOImplementation(
         return outputQueue.peekLast()
     }
 
-    @Deprecated(
-        "Use writeLine with vararg instead",
-        replaceWith = ReplaceWith(
-            "writeValue(*line.split(\" \").toTypedArray(), append = append, lineBreak = lineBreak)"
-        ),
-        level = DeprecationLevel.WARNING
-    )
-    override fun writeLine(line: String, append: Boolean, lineBreak: Boolean) =
-        writeValue(*line.split(" ").toTypedArray(), append = append, lineBreak = lineBreak)
-
     override fun writeValue(vararg values: Any, append: Boolean, lineBreak: Boolean) {
         if (!append && outputQueue.peekLast().isNotEmpty()) outputQueue.add(LinkedList())
 
@@ -70,41 +46,6 @@ internal class IOImplementation(
 
         if (lineBreak) outputQueue.add(LinkedList())
     }
-
-    @Deprecated(
-        "Use writeValue instead",
-        replaceWith = ReplaceWith("writeValue(value, lineBreak = lineBreak)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun writeInt(value: Int, lineBreak: Boolean) = writeValue(value, lineBreak = lineBreak)
-
-    @Deprecated(
-        "Use writeValue instead",
-        replaceWith = ReplaceWith("writeValue(*values.toTypedArray(), lineBreak = lineBreak)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun writeInts(vararg values: Int, lineBreak: Boolean) = writeValue(*values.toTypedArray(), lineBreak = lineBreak)
-
-    @Deprecated(
-        "Use writeValue instead",
-        replaceWith = ReplaceWith("writeValue(value, lineBreak = lineBreak)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun writeDouble(value: Double, lineBreak: Boolean) = writeValue(value, lineBreak = lineBreak)
-
-    @Deprecated(
-        "Use writeValue instead",
-        replaceWith = ReplaceWith("writeValue(*values.toTypedArray(), lineBreak = lineBreak)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun writeDoubles(vararg values: Double, lineBreak: Boolean) = writeValue(*values.toTypedArray(), lineBreak = lineBreak)
-
-    @Deprecated(
-        "Use writeValue instead",
-        replaceWith = ReplaceWith("writeValue(lineBreak = true)"),
-        level = DeprecationLevel.WARNING
-    )
-    override fun writeBreak() = writeValue(lineBreak = true)
 
     override fun repeat() {
         filterEmptyInput()
