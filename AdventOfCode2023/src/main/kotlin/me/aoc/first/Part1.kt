19a54c71a04f19584934f2f75b15f3070b0553f7
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-12-01 09:41:36 +0100

    AoC 1.

---
+++ AdventOfCode2023/src/main/kotlin/me/aoc/first/Part1.kt
@@ -0,0 +1,28 @@
+package me.aoc.first
+
+import me.task.Task
+import me.task.source.standard.file
+import me.task.source.standard.string
+
+fun main() = Task({
+    string("""
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent()) {
+        expected = "142"
+    }
+    file("AdventOfCode2023/in/first/part1.in")
+}) {
+    var value = 0
+    while (hasNextLine()) {
+        val line = readValue(endOfLine = true)
+
+        val firstDigit = line.first { it.isDigit() }.toString().toInt()
+        val lastDigit = line.last { it.isDigit() }.toString().toInt()
+
+        value += firstDigit * 10 + lastDigit
+    }
+    writeValue(value)
+}
