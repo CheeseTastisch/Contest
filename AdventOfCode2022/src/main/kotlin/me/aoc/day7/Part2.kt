1ccd720aafa114a8045a73f281f612764c5fb6e2
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-12-05 16:07:08 +0100

    feat(AoC2022): 7th

---
+++ AdventOfCode2022/src/main/kotlin/me/aoc/day7/Part2.kt
@@ -0,0 +1,69 @@
+package me.aoc.day7
+
+import me.task.Task
+import me.task.source.standard.file
+import me.task.source.standard.string
+
+fun main() = Task({
+    string("""
+        ${'$'} cd /
+        ${'$'} ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        ${'$'} cd a
+        ${'$'} ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        ${'$'} cd e
+        ${'$'} ls
+        584 i
+        ${'$'} cd ..
+        ${'$'} cd ..
+        ${'$'} cd d
+        ${'$'} ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k
+    """.trimIndent()) {
+        expected = "24933642"
+    }
+    file("AdventOfCode2022/in/day7/part2.in")
+}) {
+    var current = ""
+    val root = Directory("/", mutableListOf())
+
+    while (hasNextLine()) {
+        if (readValue(peek = true).startsWith("$")) {
+            readValue()
+            if (readValue() == "cd") {
+                val to = readValue()
+                current = when(to) {
+                    "/" -> "/"
+                    ".." -> current.split("/").dropLast(1).joinToString("/")
+                    else -> "$current/$to"
+                }
+            }
+
+            readValues()
+            continue
+        }
+
+        val currentDirectory = if (current == "/") root else root.getDirectory(current)!!
+
+        val (type, name) = readValues()
+        if (name in currentDirectory) continue
+
+        currentDirectory.entries.add(
+            if (type == "dir") Directory(name, mutableListOf())
+            else File(name, type.toInt())
+        )
+    }
+
+    val missingSpace = 30000000 - 70000000 + root.size
+    writeValue(root.recursiveSubdirectories.filter { it.size >= missingSpace }.minBy { it.size }.size)
+}
