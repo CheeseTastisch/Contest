db6f05e02a38d97fa1064630895e145b1bc8fc95
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-12-05 13:16:47 +0100

    feat(AoC2022): 4th

---
+++ AdventOfCode2022/src/main/kotlin/me/aoc/day4/Part1.kt
@@ -0,0 +1,36 @@
+package me.aoc.day4
+
+import me.task.Task
+import me.task.source.standard.file
+import me.task.source.standard.string
+
+fun main() = Task({
+    string(
+        """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+    """.trimIndent()
+    ) {
+        expected = "2"
+    }
+    file("AdventOfCode2022/in/day4/part1.in")
+}) {
+    var score = 0
+
+    while (hasNextLine()) {
+        val (range1, range2) = readValue(endOfLine = true)
+            .split(",")
+            .map { it.split("-") }
+            .map { (start, end) -> start.toInt() to end.toInt() }
+
+
+        if (range1.first >= range2.first && range1.second <= range2.second
+            || range2.first >= range1.first && range2.second <= range1.second) score++
+    }
+
+    writeValue(score)
+}
