725fb579f2a9aafc679519d90c832069ba93abe2
lian <lian.hoerschlaeger@gmail.com>
2024-04-19 14:52:22 +0200

    feat: 39th school ccc

--- CCC2024Spring/src/main/kotlin/me/goldentrio/school/Level2.kt
+++ CCC2024Spring/src/main/kotlin/me/goldentrio/school/Level2.kt
@@ -5,12 +5,37 @@ import me.task.source.standard.directory
 import me.task.source.standard.file
 
 fun main() = Task({
-    file("CCC2024Spring/school/lvl2/level2_example.in") {
-        expected("CCC2024Spring/school/lvl2/level2_example.out")
-    }
-//    directory("CCC2024Spring/school/lvl2") {
-//        expected("level2_example.in", "level2_example.out")
+//    file("CCC2024Spring/school/lvl2/level2_example.in") {
+//        expected("CCC2024Spring/school/lvl2/level2_example.out")
 //    }
+    directory("CCC2024Spring/school/lvl2") {
+        expected("level2_example.in", "level2_example.out")
+    }
 }) {
+    readValues()
+    readValues()
+    readValues()
 
+    while (hasNextLine()) {
+        val coins = readInts()
+        val amounts = readInts()
+
+        for(amount in amounts) {
+            val (coin1, coin2) = getCoins(coins, amount)
+            writeValue(coin1, coin2, lineBreak = true)
+        }
+    }
+}
+
+private fun getCoins(coins: List<Int>, amount: Int): Pair<Int, Int> {
+    val missing = mutableMapOf<Int, Int>()
+
+    for (coin in coins) {
+        if (coin in missing) return missing[coin]!! to coin
+        if (coin * 2 == amount) return coin to coin
+
+        missing[amount - coin] = coin
+    }
+
+    throw IllegalArgumentException("No coins found")
 }
--- README.md
+++ README.md
@@ -6,24 +6,28 @@ including coding contests or the advent of code.
 
 ## Cloudflight Coding Contest
 - [38th School CCC (Autumn 2023)](CCC2023Autumn/src/main/kotlin/me/goldentrio/school)
+  - Team: The Golden Trio
   - Location: Traun (HTBLA Traun)
   - Completed Levels: 4 (3 in Contest)
   - Total Time: _Hidden due to an error in the validation system_
   - Local Rank: 1
   - Global Rank: _Hidden due to an error in the validation system_
 - [38th Classic CCC (Autumn 2023)](CCC2023Autumn/src/main/kotlin/me/goldentrio/classic)
+  - Team: The Golden Trio
   - Location: Linz (JKU Linz)
   - Completed Levels: 4 (4 in Contest)
   - Total Time: 00:48:23
   - Local Rank: 16
   - Global Rank: 156
 - [39th School CCC (Spring 2024)](CCC2024Spring/src/main/kotlin/me/goldentrio/school)
+  - Team: The Golden Trio
   - Location: Vienna (City Hall Vienna)
-  - Completed Levels: ? (? in Contest)
-  - Total Time: ?
-  - Local Rank: ?
-  - Global Rank: ?
+  - Completed Levels: 4 (3 in Contest)
+  - Total Time: 00:24:04
+  - Local Rank: 4
+  - Global Rank: 26
 - [39th Classic CCC (Spring 2024)](CCC2024Spring/src/main/kotlin/me/goldentrio/classic)
+  - Team: The Golden Trio
   - Location: Vienna (City Hall Vienna)
   - Completed Levels: ? (? in Contest)
   - Total Time: ?
