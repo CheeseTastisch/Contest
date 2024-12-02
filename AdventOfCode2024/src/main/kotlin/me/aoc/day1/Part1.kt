75f4c9f61754e85d674469aace01218409a6b8fc
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2024-12-02 12:29:23 +0100

    AoC Day 1

---
+++ AdventOfCode2024/src/main/kotlin/me/aoc/day1/Part1.kt
@@ -0,0 +1,36 @@
+package me.aoc.day1
+
+import me.task.Task
+import me.task.source.standard.inferFile
+import me.task.source.standard.string
+import kotlin.math.absoluteValue
+
+fun main() = Task({
+    string("""
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    """.trimIndent()) {
+        valueSplit = "   "
+        expected = "11"
+    }
+
+    inferFile("day1/part1.in") {
+        valueSplit = "   "
+    }
+}) {
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+    while (hasNextLine()) {
+        list1.add(readInt())
+        list2.add(readInt(endOfLine = true))
+    }
+
+    list1.sort()
+    list2.sort()
+
+    writeValue((list1.indices).fold(0) { acc, i -> acc + (list1[i] - list2[i]).absoluteValue })
+}
---
+++ AdventOfCode2024/src/main/kotlin/me/aoc/day1/Part2.kt
@@ -0,0 +1,36 @@
+package me.aoc.day1
+
+import me.task.Task
+import me.task.source.standard.inferFile
+import me.task.source.standard.string
+
+fun main() = Task({
+    string("""
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    """.trimIndent()) {
+        valueSplit = "   "
+        expected = "31"
+    }
+
+    inferFile("day1/part2.in") {
+        valueSplit = "   "
+    }
+}) {
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+
+    while (hasNextLine()) {
+        list1.add(readInt())
+        list2.add(readInt(endOfLine = true))
+    }
+
+    writeValue((list1.indices).fold(0) { acc, i ->
+        val number = list1[i]
+        acc + list2.count { it == number } * number
+    })
+}
